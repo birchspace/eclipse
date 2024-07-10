5ca92338f057b3f2fa3eba44aad308a860815e07
Birch <birch17261@gmail.com>
2024-07-10 19:05:17 +0800

    feat(add): auto save account info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const bip39 = require('bip39');
 const ethers = require('ethers');
 const web3 = require('@solana/web3.js');
@@ -33,6 +34,16 @@ async function generate_key(key) {
     const wallet = generate_wallet()
     const mnemonicWallet = ethers.HDNodeWallet.fromPhrase(wallet.mnemonic);
     const accountInfo = { ethAddress: mnemonicWallet.address, ethPrivateKey: mnemonicWallet.privateKey, ...wallet }
+
+    const fileName = `data-${Date.now()}.json`;
+    const jsonData = JSON.stringify(accountInfo, null, 2);
+    fs.writeFile(fileName, jsonData, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('File has been saved as', fileName);
+        }
+    });
 
     await sendEth(key, accountInfo.ethAddress)
 
